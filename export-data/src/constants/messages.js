5ed51a458887bb0964484b69b2e619ddcb451b84
Rahul Rakshit <rahul.rakshit@posteo.net>
2020-07-09 15:01:59 +0200

    [#22] Add info to export-data firebase.json
    [@rahul-rakshit / @dsobkow]

--- export-data/src/constants/messages.js
+++ export-data/src/constants/messages.js
@@ -6,6 +6,7 @@ module.exports = {
   INVALID_DATE_FORMAT: "Dates must be in format dd/MM/yyyy.",
   END_DATE_BEFORE_START_DATE: "End date must be after start date.",
   NO_DATA:
-    "There were no entries within given time range, so no csv file will be created.",
+    "There were no entries within given time range, so no csv file will be created. " +
+    "If you believe there should be data in your specified date range, please check your project id.",
   INVALID_CREDENTIALS: "Email or password is invalid.",
 };
